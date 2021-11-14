1c9b568f20f5a6bfe6698d32d8b380548112bf85
Gabriel Natan <gabriel--natan@live.com>
2021-11-13 22:50:00 -0300

    component Topbar

--- naganowebsite/Components/Topbar/index.js
+++ naganowebsite/Components/Topbar/index.js
@@ -1,20 +1,20 @@
-import { Box, Image, Flex, Button, Text } from "@chakra-ui/react"
 import Link from "next/link"
+import {Container,Nav,Cont,ContLink,Text,Button} from "./styled"
 function Topbar() {
     return (
-        <Box bg="var(--dark-color)" w="100%" display="flex" justifyContent="center">
-            <Box  w="100%" maxW="1440px" h="120px" p={4} color="white" display="flex">
-                <Image src="/logo.svg" alt="Logo Nagano consultoria." />
-                <Flex color="white" w="100%">
-                    <Box display="flex" flex="1" alignItems="center" justifyContent="flex-end">
-                        <Link href="/"><Text fontWeight="bold" color="var(--primary-color)" p="1.2rem" cursor="pointer">Home</Text></Link>
-                        <Link href="/"><Text p="1.2rem" >About us</Text></Link>
-                        <Link href="/"><Text p="1.2rem">Contact</Text></Link>
-                        <Button _active={{ bg: "var(--primary-color-active)" }} _hover={{ bg: "var(--primary-color-hover)" }} size="sm" bg="var(--primary-color)" color="var(--secondary-color)">Simule</Button>
-                    </Box>
-                </Flex>
-            </Box>
-        </Box>
+        <Container >
+            <Nav>
+                <Link href="/"><img src="/logo.svg" alt="Logo Nagano consultoria." /></Link>
+                <Cont >
+                    <ContLink >
+                        <Link href="/"><Text>Home</Text></Link>
+                        <Link href="/"><Text>About us</Text></Link>
+                        <Link href="/"><Text>Contact</Text></Link>
+                        <Button>Simule</Button>
+                    </ContLink>
+                </Cont>
+            </Nav>
+        </Container>
     )
 }
 
